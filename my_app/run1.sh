79fbacdfd00d2e4a05bc00a93431dfa245191a49
“Zakiya” <zakiyafebin@gmail.com>
2025-06-12 18:17:03 +1000

    Update terraform and Docker configurations

--- my_app/run1.sh
+++ my_app/run1.sh
@@ -1,7 +1,7 @@
 git status
 git add .
 #git add ../Jenkinsfile ../my_app/Dockerfile 
-git add ../terraform/main.tf 
-# git add ../terraform/MyKeyPair.pub
+# git add ../terraform/main.tf 
+git add  ./MyKeyPair.pub
 git commit -m "Update terraform and Docker configurations"
 git push origin main
